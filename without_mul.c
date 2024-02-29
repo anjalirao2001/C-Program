765d5073bb70039a125b98ddf569029bdbc177ce
Kumari Anjali Rao <118367818+anjalirao2001@users.noreply.github.com>
2024-02-29 06:18:30 +0530

    Multiplication by 2 without multiply operator

---
+++ without_mul.c
@@ -0,0 +1,13 @@
+
+//Multiply an Integer Number by 2 Without Using Multiplication Operator
+
+#include<stdio.h>
+
+int main()
+{
+   int x;
+   printf("Enter a number: ");
+   scanf("%d",&x);
+   printf("%d", x<<1);
+   return 0;
+}
